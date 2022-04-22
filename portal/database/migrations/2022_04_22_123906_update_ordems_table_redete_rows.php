36bb0ae7e27c20dab30cd508908536503cfcb2fc
Azafo Alexandre Cossa <azafoalexandre@outlook.com>
2022-04-22 14:44:40 +0200

    remove unnecessary fields

---
+++ portal/database/migrations/2022_04_22_123906_update_ordems_table_redete_rows.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateOrdemsTableRedeteRows extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ordems', function(Blueprint $table){
+            $table->dropForeign(['projecto_id']);
+            $table->dropForeign(['viatura_id']);
+            $table->dropColumn('projecto_id');
+            $table->dropColumn('viatura_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
