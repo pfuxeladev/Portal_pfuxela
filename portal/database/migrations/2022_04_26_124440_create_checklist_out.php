594c817ce2b0543d296cbc546d6d93b32eeef7ec
Azafo Cossa <azafosocial@gmail.com>
2022-05-04 16:50:41 +0200

    checklists

--- portal/database/migrations/2022_04_26_124440_create_checklist_out.php
+++ portal/database/migrations/2022_04_26_124440_create_checklist_out.php
@@ -19,6 +19,10 @@ class CreateChecklistOut extends Migration
             $table->id();
             $table->foreignIdFor(Viatura::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignIdFor(motorista::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->boolean('carta_conducao')->default(false);
+            $table->bigInteger('km_inicio');
+            $table->time('hr_inicio')->nullable();
+            $table->boolean('uniforme')->default(false);
             $table->string('limpezaState');
             $table->string('vasoEspansorState');
             $table->string('LiquidoVidroState');
@@ -27,8 +31,23 @@ class CreateChecklistOut extends Migration
             $table->string('OleoTravoesState');
             $table->string('ACState');
             $table->string('SistemaABS_State');
-            $table->string('CintoSeguracaState');
-            $table->string('destino');
+            $table->boolean('CintoSeguracaState')->default(true);
+            $table->string('tipo_saida');
+            $table->boolean('motorista_dss')->default(false);
+            $table->boolean('lista_presenca')->default(false);
+            $table->boolean('colete_saida')->default(false);
+            $table->boolean('pneu_sobr_saida')->default(false);
+            $table->boolean('macaco_saida')->default(false);
+            $table->boolean('inspencao_saida')->default(false);
+            $table->boolean('triangulo_saida')->default(false);
+            $table->boolean('chave_roda_saida')->default(false);
+            $table->boolean('kit_reboque_saida')->default(false);
+            $table->boolean('kit_1_socorros_saida')->default(false);
+            $table->boolean('extintor_saida')->default(false);
+            $table->boolean('livrete_saida')->default(false);
+            $table->boolean('licenca_saida')->default(false);
+            $table->boolean('seguros_saida')->default(false);
+            $table->boolean('taxaradio_saida')->default(false);
             $table->timestamps();
         });
     }
