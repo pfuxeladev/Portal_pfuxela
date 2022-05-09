c939770d016d62a2e781dbc577aec26522edd1fb
Azafo Cossa <azafosocial@gmail.com>
2022-05-09 07:51:24 +0200

    Initial validation to checkout vehicles

--- portal/app/Http/Controllers/ChecklistController.php
+++ portal/app/Http/Controllers/ChecklistController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\checkListIn;
 use App\Models\CheckListOut;
+use App\Models\motorista;
+use App\Models\Viatura;
 use Illuminate\Http\Request;
 
 class ChecklistController extends Controller
@@ -18,6 +20,19 @@ class ChecklistController extends Controller
     }
 
     public function saveChacklistOut(Request $request){
-        
+        $viatuara = Viatura::where('id', 'viatura_id')->first();
+        $motorista = motorista::where('id', 'motorista_id')->first();
+
+        if(empty($motorista)){
+            return response()->json(['error' => 'O motorista não foi encontrado!'], 404);
+        }
+
+        if(empty($viatuara)){
+            return response()->json(['error' => 'A viatura não foi encontrada!'], 404);
+        }
+
+        if($viatuara->estado == false || $viatuara->locate == 'OUT'){
+            return response()->json(['error' => 'A viatura desejada não se encontra disponível!'], 404);
+        }
     }
 }
