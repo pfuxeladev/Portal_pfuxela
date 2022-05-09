734568a596a72afcff7587ac598419ba24b4cebc
pfuxeladev <leochima@gmail.com>
2022-05-09 06:22:26 +0200

    checklistCreated

---
+++ portal/app/Http/Controllers/API/Back/CheckListInController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API\Back;
+
+use App\Http\Controllers\Controller;
+use App\Models\CheckListIn;
+use Illuminate\Http\Request;
+
+class CheckListInController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CheckListIn  $checkListIn
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CheckListIn $checkListIn)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CheckListIn  $checkListIn
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CheckListIn $checkListIn)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CheckListIn  $checkListIn
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CheckListIn $checkListIn)
+    {
+        //
+    }
+}
---
+++ portal/app/Http/Controllers/API/Back/CheckListOutController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\API\Back;
+
+use App\Http\Controllers\Controller;
+use App\Models\CheckListOut;
+use App\Models\motorista;
+use App\Models\Viatura;
+use Illuminate\Http\Request;
+
+class CheckListOutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+   function ListViaturas(){
+       return Viatura::all();
+   }
+
+   function ListMotoristas(){
+       return motorista::join('people', 'motoristas.person_id', '=', 'people.id')->select('people.nome_completo as nome', 'motoristas.id')->orderBy('id','desc')->get();
+   }
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CheckListOut  $checkListOut
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CheckListOut $checkListOut)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CheckListOut  $checkListOut
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CheckListOut $checkListOut)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CheckListOut  $checkListOut
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CheckListOut $checkListOut)
+    {
+        //
+    }
+}
