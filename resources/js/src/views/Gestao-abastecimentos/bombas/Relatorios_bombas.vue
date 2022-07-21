8c04c7f033fadc95b24b94c7eb2a59d8bd456286
pfuxeladev <supportdesk@pfuxela.co.mz>
2022-07-21 22:02:55 +0200

    gerar relatorio

---
+++ resources/js/src/views/Gestao-abastecimentos/bombas/Relatorios_bombas.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="invoice-add-wrapper mt-3">
+    <b-row class="invoice-add">
+      <b-col cols="12">
+        <b-card no-body> </b-card>
+      </b-col>
+    </b-row>
+  </section>
+</template>
+<script>
+import {
+  BCard,
+  BCardHeader,
+  BCardBody,
+  BCardFooter,
+  BBadge,
+  BLink,
+  BRow,
+  BCol,
+  BListGroup,
+  BListGroupItem,
+  BImg,
+  BFormGroup,
+  BModal,
+  // eslint-disable-next-line import/newline-after-import
+} from "bootstrap-vue";
+import { useToast } from "vue-toastification/composition";
+import ToastificationContent from "@core/components/toastification/ToastificationContent.vue";
+import { ref, onUnmounted } from "@vue/composition-api";
+import moment from "moment";
+import Form from "vform";
+import store from "@/store";
+import router from "@/router"
+
+export default {
+  components: {
+    BCard,
+    BCardHeader,
+    BCardBody,
+    BCardFooter,
+    BBadge,
+    BLink,
+    BRow,
+    BCol,
+    BListGroup,
+    BListGroupItem,
+    BImg,
+    BFormGroup,
+    BModal,
+  },
+  setup() {
+    const bomba = ref(null);
+
+    const toast = useToast();
+  },
+};
+</script>
