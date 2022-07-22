29f23b7a823f34555b46e4fd7ca1b99a6830938e
pfuxeladev <supportdesk@pfuxela.co.mz>
2022-07-22 16:12:38 +0200

    checkout vel

--- app/Http/Controllers/API/Back/AbastecimentoController.php
+++ app/Http/Controllers/API/Back/AbastecimentoController.php
@@ -103,7 +103,7 @@ class AbastecimentoController extends Controller
 
             $datetime = \Carbon\Carbon::now()->subHours(5)->format("Y-m-d H:i:s");
         $viatura = Viatura::where('id', $request->viatura_id)->first();
-        $lastOrderViatura = ordem_viatura::join('viaturas', 'viaturas.id', '=', 'ordem_viaturas.viatura_id')->whereRaw('ordem_viaturas.created_at', '>=', $datetime)->where('viaturas.id', $request->viatura_id)->first();
+        $lastOrderViatura = ordem_viatura::join('viaturas', 'viaturas.id', '=', 'ordem_viaturas.viatura_id')->where('ordem_viaturas.created_at', '>', $datetime)->where('viaturas.id', $request->viatura_id)->first();
         // return $lastOrderViatura;
         if (!empty($lastOrderViatura)) {
             return response()->json(['erro' => 'Erro! Essa viatura ja foi abastecida contacte o administrador ou faça abastecimento extraordinario'], 421);
