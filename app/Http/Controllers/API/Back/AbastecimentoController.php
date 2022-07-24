fc71b32c545a981915e62ccc3534cf52aa1f023f
pfuxeladev <supportdesk@pfuxela.co.mz>
2022-07-24 12:11:21 -0500

    abastecimento rota

--- app/Http/Controllers/API/Back/AbastecimentoController.php
+++ app/Http/Controllers/API/Back/AbastecimentoController.php
@@ -128,11 +128,11 @@ class AbastecimentoController extends Controller
             return response()->json(['erro' => 'A Bomba nao tem ' . $viatura->tipo_combustivel], 421);
         }
         foreach ($request->rota_id as $key => $rt) {
-            $ordem_rota = OrdemViaturaRota::join('ordem_viaturas', 'ordem_viatura_rotas.ordem_viatura_id', '=', 'ordem_viaturas.id')
-            ->join('viaturas', 'ordem_viaturas.viatura_id', '=', 'viaturas.id')->where(['ordem_viatura_rotas.rota_id' => $rt])->where('ordem_viatura_rotas.created_at','>', Carbon::now()->subHours(3)->toDateTimeString())->orWhere('viaturas.locate', 'IN')->first();
-            if (!empty($ordem_rota)) {
-                return response()->json(['erro' => 'Nao pode abastecer mais de duas viatura na mesma rota'], 421);
-            }
+            // $ordem_rota = OrdemViaturaRota::join('ordem_viaturas', 'ordem_viatura_rotas.ordem_viatura_id', '=', 'ordem_viaturas.id')
+            // ->join('viaturas', 'ordem_viaturas.viatura_id', '=', 'viaturas.id')->where(['ordem_viatura_rotas.rota_id' => $rt])->where('ordem_viatura_rotas.created_at','<', Carbon::now()->subHours(3)->toDateTimeString())->orWhere('viaturas.locate', 'IN')->first();
+            // if (!empty($ordem_rota)) {
+            //     return response()->json(['erro' => 'Nao pode abastecer mais de duas viatura na mesma rota'], 421);
+            // }
         }
 
 
