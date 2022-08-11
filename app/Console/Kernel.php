a72ef6b4be179d4f5129b3b0cbbb1d7faff53672
pfuxeladev <supportdesk@pfuxela.co.mz>
2022-08-11 20:51:05 +0200

    rotas kms

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('ReportDatasCron:cron')->everyMinute()->timezone('Africa/Maputo');
+        $schedule->command('ReportAbastecimento:cron')->everyMinute()->timezone('Africa/Maputo');
     }
 
     /**
