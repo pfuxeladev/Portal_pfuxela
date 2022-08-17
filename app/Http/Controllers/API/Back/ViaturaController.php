e6a63f271a6a7793553d025f05e505c837e1f0b5
pfuxeladev <supportdesk@pfuxela.co.mz>
2022-08-17 05:14:20 -0500

    viatura

--- app/Http/Controllers/API/Back/ViaturaController.php
+++ app/Http/Controllers/API/Back/ViaturaController.php
@@ -44,7 +44,7 @@ class ViaturaController extends Controller
     function listViatura(){
         // return Viatura::join('checklist_out', 'viaturas.id', '=', 'checklist_out.viatura_id')->where('viaturas.estado', 1)->whereDate('checklist_out.created_at', Carbon::today()->subDays( 3 ))->orWhere('updated_at', '>=', Carbon::now()->subDays(14))
         // ->orderBy('viaturas.id', 'desc')->get();
-        return Viatura::where('locate', '=', 'IN')->where('estado', true)->where('updated_at', '>=', Carbon::now()->subDays(14))
+        return Viatura::where('locate', '=', 'IN')->where('estado', true)->where('updated_at', '>=', Carbon::now()->subDays(28))
             ->select('matricula', 'id')->get();
     }
     public function store(Request $request)
