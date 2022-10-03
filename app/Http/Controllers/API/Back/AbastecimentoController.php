cfe247f0216ec8402db319115fba271478b7402a
pfuxeladev <supportdesk@pfuxela.co.mz>
2022-10-03 11:05:33 +0200

    alocar viatura

--- app/Http/Controllers/API/Back/AbastecimentoController.php
+++ app/Http/Controllers/API/Back/AbastecimentoController.php
@@ -48,7 +48,9 @@ class AbastecimentoController extends Controller
     {
         $viatura = array();
 
-        $viatura_rota = ViaturaRota::whereDate('created_at', date('Y-m-d'))->get();
+        $datetime = \Carbon\Carbon::now()->subHours(5)->format("Y-m-d H:i:s");
+
+        $viatura_rota = ViaturaRota::whereDate('created_at','>=', $datetime)->get();
 
             foreach ($viatura_rota as $key => $vr) {
                $viatura[$key] = $vr->viatura_id;
