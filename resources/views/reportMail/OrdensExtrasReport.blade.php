c0309db3f8792e6af6b4af41518ffcbe0dea85bc
pfuxeladev <supportdesk@pfuxela.co.mz>
2022-10-24 15:55:17 +0200

    view extra report

--- resources/views/reportMail/OrdensExtrasReport.blade.php
+++ resources/views/reportMail/OrdensExtrasReport.blade.php
@@ -92,5 +92,127 @@
 </head>
 
 <body style="font-family: sans-serif;">
+    <div class="container">
+        <div class="content-header">
+            <h3 class="centered-title"
+                style="text-align: center !important; color: whitesmoke; position: relative; width: 100%;">
+                Relatorio Geral de Abastecimentos Extras
+            </h3>
+        </div>
+        <div class="content">
+            <div class="logotipo">
+                <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('/images/logo.png'))) }}"
+                    width="180px">
+            </div>
+        </div>
+        <div class="row-data">
+            <?php
+            $total = 0;
+            ?>
+            <table class="table-content" style="font-size: 6pt">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Codigo</th>
+                        <th>
+                            Estado
+                        </th>
+                        <th>matricula</th>
+                        <th>cap/tanque</th>
+                        <th>ltr/km</th>
+                        <th>qtd_anterior</th>
+                        <th>qtd</th>
+                        <th>combustivel</th>
+                        <th>preco/ltr</th>
+                        <th>destino</th>
+                        <th>justificacao</th>
+                        <th>criado_por</th>
+                        <th>actualizador por</th>
+                        <th>subtotal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($MailsOrder as $key => $order)
+                        <tr>
+                            <td>{{ $order->data_criacao }}</td>
+                            <td>{{ $order->codigo_ordem }}</td>
+                            <td>{{ $order->estado }}</td>
+                            <td>
+                                @foreach ($order->viatura as $viatura)
+                                    {{ $viatura->matricula }}
+                                @endforeach
+                            </td>
+                            <td>
+                                @foreach ($order->viatura as $viatura)
+                                    {{ $viatura->capacidade_tanque }}
+                                @endforeach
+                            </td>
+                            <td>
+                                @foreach ($order->viatura as $viatura)
+                                    {{ $viatura->capacidade_media }}
+                                @endforeach
+                            </td>
+                            <td>
+                                {{ $order->qtd_ant }}
+                            </td>
+                            <td>
+                                {{ $order->qtd }}
+                            </td>
+                            <td>
+                                @foreach ($order->viatura as $viatura)
+                                    {{ $viatura->tipo_combustivel }}
+                                @endforeach
+                            </td>
+                            <td>
+                                <?php
+                                $preco_unit = $order->ordem_viatura['0']->preco_cunsumo / $order->qtd;
+                                ?>
+                                {{ $preco_unit }}
+                            </td>
+                            <td>{{ $order->destino }}</td>
+                            <td>{{ $order->descricao }}</td>
+                            <td>
+                                <?php
+                                $user = App\Models\User::where('id', $order->createdBy)->first();
+
+                                ?>
+                                {{ $user->name }}
+                            </td>
+                            <td>
+                                <?php
+                            $user = App\Models\User::where('id', $order->approvedBy)->first();
+                            if(!empty($user)){
+                                ?>
+                                {{ $user->name }}
+                                <?php
+                        }else{
+                            ?>
+                                -
+                                <?php
+                        }
+                        ?>
+                            </td>
+                            <td>
+                                {{ $order->ordem_viatura['0']->preco_cunsumo }}
+                            </td>
+                        </tr>
+                        <?php
+                        $total += $order->ordem_viatura['0']->preco_cunsumo;
+                        ?>
+                    @endforeach
+
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="14" style="text-align:right">
+                            total
+                        </td>
+                        <td>{{$total}}</td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+    </div>
 </body>
+
 </html>
